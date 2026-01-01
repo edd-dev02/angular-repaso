acda78a28b3f1b62cde056ca8fbbf084117702b0
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-01 17:56:22 -0600

    Heroes: Interpolación, One way data binding y directiva ngIf añadidos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public alterEgo: string = "Spider-man"
+  public nombre: string = "peter parker";
+  public edad: number = 17;
+
+  get capitalizedName(): string {
+    return this.nombre.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+
+  changeHero(): void {
+    this.alterEgo = "Iron-man";
+    this.nombre = "tony stark";
+  }
+
+  changeAge(): void {
+    this.edad = 45;
+  }
+
 }
