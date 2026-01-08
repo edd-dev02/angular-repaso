38dff152d148d3821488bef06c067e686d57606c
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-08 14:35:23 -0600

    Estructura del nuevo módulo creada y primer implementacion de comunicacion entre componentes (Input)

---
+++ src/app/dbz/components/addcharacter/addcharacter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './addcharacter.component.html',
+  styleUrl: './addcharacter.component.css'
+})
+export class AddCharacterComponent {
+
+}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+
+  @Input()
+  public listCharacter: Character[] = [ {name: "Trunks", power: 80}];
+
+ }
